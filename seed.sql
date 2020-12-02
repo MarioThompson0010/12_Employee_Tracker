d949351cbc2ba353c0aac44043147b170167d5b8
Mario Thompson <MarioThompson0010@gmail.com>
2020-12-02 00:17:08 -0600

    Create seed file

---
+++ seed.sql
@@ -0,0 +1,10 @@
+INSERT INTO employee (first_name, last_name, role_id)
+VALUES ('Mario', 'Thompson', 1);
+
+INSERT INTO role (title, salary, department_id)
+VALUES ('Manager', 100, 123);
+
+INSERT INTO department (name)
+VALUES ('Software Engineering');
+
+
