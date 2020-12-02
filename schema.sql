f84e26d9fdf1833764162958ea119287b01cd214
Mario Thompson <MarioThompson0010@gmail.com>
2020-12-01 23:56:32 -0600

    fix errors in schema

--- schema.sql
+++ schema.sql
@@ -6,7 +6,7 @@ CREATE DATABASE employee_trackerDB;
 USE employee_trackerDB;
 
 CREATE TABLE employee (
-    id int AUTO_INCREMNT,
+    id int AUTO_INCREMENT,
     first_name VARCHAR(30),
     last_name VARCHAR(30),
     role_id int,
@@ -15,15 +15,15 @@ CREATE TABLE employee (
 );
 
 CREATE TABLE role (
-    id int AUTO_INCREMNT,
+    id int AUTO_INCREMENT,
     title VARCHAR(30),
     salary decimal,
-    department_id int
+    department_id int,
     PRIMARY KEY (id)
 );
 
 CREATE TABLE department (
-    id int AUTO_INCREMNT,
-    name VARCHAR(30)
+    id int AUTO_INCREMENT,
+    name VARCHAR(30),
     PRIMARY KEY (id)
 );
