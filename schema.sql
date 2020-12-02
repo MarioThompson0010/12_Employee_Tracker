d1ddec503f38beba92a92e2ca87bf183b1592d4d
Mario Thompson <MarioThompson0010@gmail.com>
2020-12-02 00:07:31 -0600

    make every field but manager_id not null

--- schema.sql
+++ schema.sql
@@ -7,23 +7,23 @@ USE employee_trackerDB;
 
 CREATE TABLE employee (
     id int AUTO_INCREMENT,
-    first_name VARCHAR(30),
-    last_name VARCHAR(30),
-    role_id int,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id int NOT NULL,
     manager_id int,
     PRIMARY KEY (id)
 );
 
 CREATE TABLE role (
     id int AUTO_INCREMENT,
-    title VARCHAR(30),
-    salary decimal,
-    department_id int,
+    title VARCHAR(30) NOT NULL,
+    salary decimal NOT NULL,
+    department_id int NOT NULL,
     PRIMARY KEY (id)
 );
 
 CREATE TABLE department (
     id int AUTO_INCREMENT,
-    name VARCHAR(30),
+    name VARCHAR(30) NOT NULL,
     PRIMARY KEY (id)
 );
